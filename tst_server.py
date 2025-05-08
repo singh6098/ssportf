b00d1655edc81e3c237ab9457515721e5d19f3ce
Sukhwinder <singh.sukhwinder90@live.com>
2025-05-08 22:58:27 +0200

    [INIT]: Portfolio website

---
+++ tst_server.py
@@ -0,0 +1,39 @@
+from flask import Flask, url_for,render_template,request, redirect
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def my_index(page_name):
+    return render_template(page_name)
+
+@app.route("/submit_form", methods=["POST","GET"])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return redirect("/thankyou.html")
+    else:
+        return "SOMETHING WENT WRONG. TRY AGAIN!"
+
+#@app.route("/about.html")
+#def my_about():
+#    return render_template('about.html')
+
+#@app.route("/works.html")
+#def my_works():
+#    return render_template('works.html')
+
+
+#@app.route("/contact.html")
+#def my_contact():
+#    return render_template('contact.html')
+
+#@app.route("/favicon.ico")
+#def IconWebsite():
+#    return render_template('sunnycastsummer.ico')
